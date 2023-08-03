268c5d34842c645be700d8ffcd6d602230b3cadf
Tago <tiagobettega.tb@gmail.com>
2023-08-03 12:50:10 -0300

    Add image and page docs

---
+++ js/docs.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuLinks = document.querySelectorAll('.menu a');
+    const contentDivs = document.querySelectorAll('#content > div');
+
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const contentId = link.getAttribute("data-content");
+
+            contentDivs.forEach((div) => div.classList.remove('active'));
+
+            document.getElementById(contentId).classList.add('active');
+        });
+    });
+});
